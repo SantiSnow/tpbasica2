efc71faef96ea1ef0f2236d489f7507c19134c0b
Leandro Gomez <leandro.ariel.gomez1@gmail.com>
2019-11-17 18:46:31 -0300

    Inicio de la clase sistema (interfaces)

--- proyectoPuntosCompras/src/src/Sistema.java
+++ proyectoPuntosCompras/src/src/Sistema.java
@@ -3,8 +3,84 @@ package src;
 import java.util.Set;
 
 public class Sistema {
+	
 	private String nombreDelSistema;
 	Set <Usuario> listaDeUsuarios;
+    Set <Producto> listaDeProducto;
+	
+    public Sistema(String nombreDelSistema, Set<Usuario> listaDeUsuarios, Set<Producto> listaDeProducto) {
+		super();
+		this.nombreDelSistema = nombreDelSistema;
+		this.listaDeUsuarios = listaDeUsuarios;
+		this.listaDeProducto = listaDeProducto;
+	}
 
+	public Sistema() {
+		super();
+	}
+
+	public String getNombreDelSistema() {
+		return nombreDelSistema;
+	}
+
+	public void setNombreDelSistema(String nombreDelSistema) {
+		this.nombreDelSistema = nombreDelSistema;
+	}
+
+	public Set<Usuario> getListaDeUsuarios() {
+		return listaDeUsuarios;
+	}
+
+	public void setListaDeUsuarios(Set<Usuario> listaDeUsuarios) {
+		this.listaDeUsuarios = listaDeUsuarios;
+	}
+
+	public Set<Producto> getListaDeProducto() {
+		return listaDeProducto;
+	}
+
+	public void setListaDeProducto(Set<Producto> listaDeProducto) {
+		this.listaDeProducto = listaDeProducto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((listaDeProducto == null) ? 0 : listaDeProducto.hashCode());
+		result = prime * result + ((listaDeUsuarios == null) ? 0 : listaDeUsuarios.hashCode());
+		result = prime * result + ((nombreDelSistema == null) ? 0 : nombreDelSistema.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sistema other = (Sistema) obj;
+		if (listaDeProducto == null) {
+			if (other.listaDeProducto != null)
+				return false;
+		} else if (!listaDeProducto.equals(other.listaDeProducto))
+			return false;
+		if (listaDeUsuarios == null) {
+			if (other.listaDeUsuarios != null)
+				return false;
+		} else if (!listaDeUsuarios.equals(other.listaDeUsuarios))
+			return false;
+		if (nombreDelSistema == null) {
+			if (other.nombreDelSistema != null)
+				return false;
+		} else if (!nombreDelSistema.equals(other.nombreDelSistema))
+			return false;
+		return true;
+	}
+    
+    
+    
 	
 }
