875f48327c4bc702ee6a4f2d0d2ba7e12bd237cb
SantiSnow <aguirresantiago81@gmail.com>
2019-11-17 18:34:18 -0300

    comentarios

--- proyectoPuntosCompras/src/interfaces/InterSistema1.java
+++ proyectoPuntosCompras/src/interfaces/InterSistema1.java
@@ -8,12 +8,13 @@ public interface InterSistema1 {
 	//esta interfaz esta dedicada a proveer cuerpo a los metodos
 	//relacionados con los usuarios
 	
-		Boolean agregaUnUsuarioAlSistema(Usuario administrador);
-		Boolean agregarUnUsuarioAlSistema2(Usuario user);
-		Boolean eliminarUsuario(Usuario cliente);
-		Boolean eliminarUsuarioHash(Usuario cliente);
-		Boolean loggearseComoUsuarioHash(String nombreUsuario, String contraseña);
-		Boolean logearseComoUsuario(String nombreUsuario, String contraseña);
-		Integer conocerCantidadDePuntos(String nombreUsuario, String contraseña);
-		Boolean salirDelSistema(String nombreDeUsuario, String contraseña);
+	Boolean agregaUnUsuarioAlSistema(Usuario administrador);
+	Boolean agregarUnUsuarioAlSistema2(Usuario user);
+	Boolean eliminarUsuario(Usuario cliente);
+	Boolean eliminarUsuarioHash(Usuario cliente);
+	Boolean loggearseComoUsuarioHash(String nombreUsuario, String contraseña);
+	Boolean logearseComoUsuario(String nombreUsuario, String contraseña);
+	Integer conocerCantidadDePuntos(String nombreUsuario, String contraseña);
+	Boolean salirDelSistema(String nombreDeUsuario, String contraseña);
+	
 }
