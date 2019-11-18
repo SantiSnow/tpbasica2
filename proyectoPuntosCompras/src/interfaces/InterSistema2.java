8e22a897a40e16b0eaeb8b2670ada46a83ef2749
SantiSnow <aguirresantiago81@gmail.com>
2019-11-17 21:26:46 -0300

    Cambiamos atributos y metodos

--- proyectoPuntosCompras/src/interfaces/InterSistema2.java
+++ proyectoPuntosCompras/src/interfaces/InterSistema2.java
@@ -4,7 +4,7 @@ public interface InterSistema2 {
 	//interfaz que define metodos de devolucion de datos del sistema
 	//tales como ver la cantidad de usuarios
 	//y vaciar las listas
-	Integer conocerTamañoDelSistemaDeUsuarios(String nombreUsuario, String contraseña);
-	Integer conocerTamañoDelSistemaDeCompras(String nombreUsuario, String contraseña);
+	Integer conocerTamañoDelSistemaDeUsuarios();
+	Integer conocerTamañoDelSistemaDeCompras();
 	String vaciarListaDeUsuarios();
 }
--- proyectoPuntosCompras/src/src/Sistema.java
+++ proyectoPuntosCompras/src/src/Sistema.java
@@ -3,17 +3,21 @@ package src;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Sistema {
+import interfaces.InterSistema1;
+import interfaces.InterSistema2;
+import interfaces.InterSistema3;
+
+public class Sistema implements InterSistema1, InterSistema2, InterSistema3{
 	//atributos
 	private String nombreDelSistema;
-	Set <Usuario> listaDeUsuarios;
-    Set <Producto> listaDeProducto;
+	public Set <Usuario> listaDeUsuarios;
+    public Set <Compra> listaDeCompra;
 	
     //constructor
     public Sistema(String nombreDelSistema) {
 		this.nombreDelSistema = nombreDelSistema;
 		listaDeUsuarios = new HashSet<Usuario>();
-		listaDeProducto = new HashSet<Producto>();
+		listaDeCompra = new HashSet<Compra>();
 	}
 
     //constructor por defecto
@@ -33,19 +37,13 @@ public class Sistema {
 	public void setListaDeUsuarios(Set<Usuario> listaDeUsuarios) {
 		this.listaDeUsuarios = listaDeUsuarios;
 	}
-	public Set<Producto> getListaDeProducto() {
-		return listaDeProducto;
-	}
-	public void setListaDeProducto(Set<Producto> listaDeProducto) {
-		this.listaDeProducto = listaDeProducto;
-	}
 
 	//hashcode e equals
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((listaDeProducto == null) ? 0 : listaDeProducto.hashCode());
+		result = prime * result + ((listaDeCompra == null) ? 0 : listaDeCompra.hashCode());
 		result = prime * result + ((listaDeUsuarios == null) ? 0 : listaDeUsuarios.hashCode());
 		result = prime * result + ((nombreDelSistema == null) ? 0 : nombreDelSistema.hashCode());
 		return result;
@@ -60,10 +58,10 @@ public class Sistema {
 		if (getClass() != obj.getClass())
 			return false;
 		Sistema other = (Sistema) obj;
-		if (listaDeProducto == null) {
-			if (other.listaDeProducto != null)
+		if (listaDeCompra == null) {
+			if (other.listaDeCompra != null)
 				return false;
-		} else if (!listaDeProducto.equals(other.listaDeProducto))
+		} else if (!listaDeCompra.equals(other.listaDeCompra))
 			return false;
 		if (listaDeUsuarios == null) {
 			if (other.listaDeUsuarios != null)
@@ -77,8 +75,73 @@ public class Sistema {
 			return false;
 		return true;
 	}
-    
-    
-    
+
+	@Override
+	public Integer conocerTamañoDelSistemaDeUsuarios() {
+		return listaDeUsuarios.size();
+	}
+
+	@Override
+	public Integer conocerTamañoDelSistemaDeCompras() {
+		return listaDeCompra.size();
+	}
+
+	@Override
+	public String vaciarListaDeUsuarios() {
+		listaDeUsuarios.clear();
+		return "La lista ha sido vaciada";
+	}
+
+	@Override
+	public Boolean agregaUnUsuarioAlSistema(Usuario administrador) {
+		
+		return null;
+	}
+
+	@Override
+	public Boolean agregarUnUsuarioAlSistema2(Usuario user) {
+		
+		return null;
+	}
+
+	@Override
+	public Boolean eliminarUsuario(Usuario cliente) {
+		
+		return null;
+	}
+
+	@Override
+	public Boolean eliminarUsuarioHash(Usuario cliente) {
+		
+		return null;
+	}
+
+	@Override
+	public Boolean loggearseComoUsuarioHash(String nombreUsuario, String contraseña) {
+		
+		return null;
+	}
+
+	@Override
+	public Boolean logearseComoUsuario(String nombreUsuario, String contraseña) {
+		
+		return null;
+	}
+
+	@Override
+	public Integer conocerCantidadDePuntos(String nombreUsuario, String contraseña) {
+		
+		return null;
+	}
+
+	@Override
+	public Boolean salirDelSistema(String nombreDeUsuario, String contraseña) {
+		
+		return null;
+	}
+	@Override
+	public void realizarUnaCompra(Usuario usuario, Producto producto) {
+		
+	}
 	
 }
