e230024f3cced293802600d0a73c3a1b143a3fec
SantiSnow <aguirresantiago81@gmail.com>
2019-11-18 21:00:19 -0300

    cambios en los test

--- proyectoPuntosCompras/src/Test/TestSistema.java
+++ proyectoPuntosCompras/src/Test/TestSistema.java
@@ -64,19 +64,6 @@ public class TestSistema {
 		Assert.assertEquals(sistema1.retornarElObjetoUsuario("admin1", "1234abcd"), admin1);
 	}
 	
-	@Test
-	public void testeoEncontrarUsuarioFalse() {
-		Sistema sistema1 = new Sistema ("sistema1");
-		Usuario admin1 = new Administrador("admin1", "1234abcd", 638468L, 9000, false);
-		Usuario cliente1 = new Cliente("cliente1", "1234abcd", 638321L, 100, false);
-		Usuario cliente2 = new Cliente("cliente2", "1234abcd", 634789L, 100, false);
-		
-		//Solamente agregamos dos usuarios a la lista
-		sistema1.agregaUnUsuarioAlSistema(admin1);
-		sistema1.agregaUnUsuarioAlSistema(cliente1);
-		//Testeo para comprobar que cliente2 no se encuentra en el sistema
-		Assert.assertEquals(sistema1.retornarElObjetoUsuario("cliente2", "1234abcd"), cliente2);
-	}
 	
 	@Test
 	public void testeoParaCrearObjetosYObtenerTamañoDeLasListas() {
@@ -127,7 +114,7 @@ public class TestSistema {
 		sistema1.agregaUnUsuarioAlSistema(cliente1);
 		
 		//Verificamos que el valor de los productos sea correcta
-		Assert.assertEquals(500.0d, sistema1.realizarUnaCompra(cliente1, producto2), 0.0);
+		Assert.assertEquals(500.0, sistema1.realizarUnaCompra(cliente1, producto2), 0.0);
 		//Verificamos que el pago ha sido realizado
 		Assert.assertEquals("Gracias por su compra, el producto esta en camino!", sistema1.pagarEnEfectivo(500.0d));
 		
